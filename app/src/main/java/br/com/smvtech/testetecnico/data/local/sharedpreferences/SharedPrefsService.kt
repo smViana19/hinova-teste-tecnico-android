93c2d5c1e0cb2e708828de18852adb0f18fa82f1
Samuu <samuelfilipevianadois@gmail.com>
2026-03-20 19:43:20 -0300

    feat: adiciona e implementa configs iniciais do shared prefs

---
+++ app/src/main/java/br/com/smvtech/testetecnico/data/local/sharedpreferences/SharedPrefsService.kt
@@ -0,0 +1,7 @@
+package br.com.smvtech.testetecnico.data.local.sharedpreferences
+
+interface SharedPrefsService {
+    fun saveUserData()
+    fun isSaveUserInfo()
+
+}
---
+++ app/src/main/java/br/com/smvtech/testetecnico/data/local/sharedpreferences/SharedPrefsServiceImpl.kt
@@ -0,0 +1,13 @@
+package br.com.smvtech.testetecnico.data.local.sharedpreferences
+
+import android.content.Context
+
+class SharedPrefsServiceImpl(context: Context) : SharedPrefsService {
+    override fun saveUserData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun isSaveUserInfo() {
+        TODO("Not yet implemented")
+    }
+}
