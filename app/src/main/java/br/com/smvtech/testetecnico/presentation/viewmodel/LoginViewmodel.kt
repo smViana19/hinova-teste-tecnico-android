6aa56d5bbdb54f8c0e5346a965c5f02b5e2f0785
Samuu <samuelfilipevianadois@gmail.com>
2026-03-20 14:27:58 -0300

    feat: adiciona configuracao para compilacao do hilt e strings de idioma

--- app/src/main/java/br/com/smvtech/testetecnico/presentation/viewmodel/LoginViewmodel.kt
+++ app/src/main/java/br/com/smvtech/testetecnico/presentation/viewmodel/LoginViewmodel.kt
@@ -1,4 +1,16 @@
 package br.com.smvtech.testetecnico.presentation.viewmodel
 
-class LoginViewmodel {
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewmodel @Inject constructor(): ViewModel() {
+    private val _document = mutableStateOf("")
+    val document: MutableState<String> = _document
+
+    private val _password = mutableStateOf("")
+    val password: MutableState<String> = _password
 }
